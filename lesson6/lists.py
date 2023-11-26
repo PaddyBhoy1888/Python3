82c7481a2935ac006a4ee6058e9b7f54e469fa75
PaddyBhoy1888 <paddylltoi@hotmail.co.uk>
2023-11-26 10:51:53 +0000

    Update 2 26.11.23

--- lesson6/lists.py
+++ lesson6/lists.py
@@ -32,7 +32,7 @@ print(users[-1])
 
 # WORKING OUT POSITIONS USING VARIABLES IN LISTS
 print(users.index('Sara'))
-# To work out the position of the variable inside a list we can use the .index
+# To work out the position of the variable inside a list we can use the '.index'
 print(users.index('Dave'))
 # To work out the position of the variable inside a list we can use the .index
 # print(users.index('Paddy'))
@@ -49,3 +49,26 @@ print(users[1:])
 print(users[-3:-1])
 # This will print from -3 to -1. This is similiar to the first range exapmle in that it starts from
 # -3 i.e. 3 from last & prints up to but not including the last.
+
+# LENGTH OF A LIST
+print(len(emptylist))
+# The 'len' function allows for the length of the list to be known.
+
+# ADD TO AN EXISTING LIST
+users.append('Paddy')
+# Using the '.append' function we can add to an existing list called users.
+print(users)
+# prints the list to confirm user has been added.
+
+# ADD TWO LISTS TOGETHER
+users += ['Jason']
+# The '+=' function allows the existing users list to be added to a new list created ['Jason']. Make sure []
+# or it will add all letters as a list.
+print(users)
+# prints the list to confirm lists have been added together into one list and contains all users.
+
+# EXTENDING A LIST
+users.extend(['Robert', 'Jimmy'])
+# Using the '.extend' function we can extend an existing list with a new list.
+print(users)
+# prints the list to confirm lists have been added together into one list and contains all users.
