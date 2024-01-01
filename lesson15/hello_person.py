e505b4ed10291812eee0209271754d6fad8281c6
PaddyBhoy1888 <paddylltoi@hotmail.co.uk>
2024-01-01 14:12:17 +0000

    update5 1.1.24

---
+++ lesson15/hello_person.py
@@ -0,0 +1,34 @@
+# Using modules and arguments
+
+
+def hello(name, lang):  # Derfin function 'hello' with two arguments to be passed
+    greetings = {  # Creates a dictionary with 3No. key/value pairs
+        "English": "Hello",
+        "Spanish": "Hola",
+        "German": "Hallo"
+    }
+
+    msg = f"{greetings[lang]} {name}!"
+    print(msg)
+
+
+if __name__ == '__main__':
+    import argparse
+
+    parser = argparse.ArgumentParser(
+        description="Provides a personal greeting."
+    )
+
+    parser.add_argument(
+        "-n", "--name", metavar="name",
+        required=True, help="The name of the person to greet."
+    )
+
+    parser.add_argument(
+        "-l", "--lang", metavar="language", required=True, choices=["English", "Spanish", "German"],
+        help="The language of the greeting."
+    )
+
+    args = parser.parse_args()
+
+    hello(args.name, args.lang)
