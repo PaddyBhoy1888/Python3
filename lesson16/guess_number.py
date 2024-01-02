d62bc0090b4ff0e961ec85c3dbf65ac4bca62c97
PaddyBhoy1888 <paddylltoi@hotmail.co.uk>
2024-01-02 13:26:55 +0000

    update2 2.1.24

--- lesson16/guess_number.py
+++ lesson16/guess_number.py
@@ -52,6 +52,11 @@ def gn(name='playerOne'):
         computer = int(computerchoice)
         # Here we change the computers choice from one of the random string values to an integer.
 
+        print(f"\n{name}, you chose {playerchoice}.")
+        # Print statement to display the players choice
+        print(f"\nI was thinking of the number {computerchoice}.\n")
+        # Print statement to display the computers choice.
+
         # define function 'decide_winner' with input parameters 'player' & 'computer'
         def decide_winner(player, computer):
             # We refer this function to the nonlocal variables listed below which are defined in the function gn local scope above.
@@ -61,16 +66,18 @@ def gn(name='playerOne'):
 
             # Using an if & else statement we can state that if the player guesses the same as the computer then the player wins. Else any other outcome the computer wins.
             if player == computer:
-                player_wins += 1  # If player wins increment the value by 1
+                player_wins += 1
+
+                # If player wins increment the value by 1
                 # Return statement to say that the player wins.
-                return f"🐱‍🏍{name}, you Win
+                return f"🥳{name}, you Win!"
 
             else:
                 python_wins += 1  # If computer wins increment the value by 1
                 # Return statement to say that the computer has won
-                return f"\n Sorry, {name}. Better luck next time.😢"
-                #A new variable to call the nested function 'decide_winner' refering to the parameters above inside the function.
-                
+                return f"Sorry, {name}. Better luck next time.😢"
+                # A new variable to call the nested function 'decide_winner' refering to the parameters above inside the function.
+
         game_result = decide_winner(player, computer)
 
         print(game_result)  # Print the output of 'game result'
@@ -80,25 +87,57 @@ def gn(name='playerOne'):
         nonlocal game_count  # Refering to the nonlocal variable
         game_count += 1  # Adding 1 everytime the game is played
         # Print out a statment that counts by converting the game_count to a string
-        print(f"\n Game count: {game_count}")
+        print(f"\nGame count: {game_count}")
         # print out the winning count.
-        print(f"\n {name}'s wins: {player_wins}")
+        print(f"\n{name}'s wins: {player_wins}")
         # print out the winning count.
-        print(f"\n python_wins: {python_wins}")
-        # Print out the winning percentage for player where percentage is calculated by (player_wins/game_count) at 2 decimal places 
+        print(f"\npython_wins: {python_wins}")
+        # Print out the winning percentage for player where percentage is calculated by (player_wins/game_count) at 2 decimal places
         print(f"\nYour winning percentage: {player_wins/game_count:.2%}")
-        # Print out the winning percentage for computer where percentage is calculated by (python_wins/game_count) at 2 decimal places 
-        print(f"\nYour winning percentage: {python_wins/game_count:.2%}")
+        # Print out the winning percentage for computer where percentage is calculated by (python_wins/game_count) at 2 decimal places
+        print(f"\nPython winning percentage: {python_wins/game_count:.2%}")
         # Print out play again statement.
         print(f"\nPlay again, {name}?")
-        
-        # To play again we provide a while loop that must be True. 
+
+        # To play again we provide a while loop that must be True.
         while True:
             # For the loop to continue the 'playagain' variable is created to allow the player to input a choice of Y or Q.
-            playagain = input("\n Y for yes or \n Q to Quit \n")
-            # If the player inputs a lower case we provide the .lower function so that python can understand the palyers input. 
+            playagain = input("\nY for yes or \nQ to Quit \n")
+            # If the player inputs a lower case we provide the .lower function so that python can understand the palyers input.
             if playagain.lower() not in ["y", "q"]:
                 continue
             else:
                 break
             # An if else statement is created so that unless the player choses one of the two inputs the question will be re asked until it receives the correct input.
+
+        if playagain.lower() == "y":
+            # Create a if statement so if the player uses lower case choice the game still understands the input and if y chosen then we wish to replay game
+            return play_gn()  # if the player choses y then game will restart and play again by calling the play_gn function
+        else:  # else statement to begin the stop process
+            print("\n🥳🎉\n")
+            print("Thank you for playing!\n")
+            # This will exit the game and return to the terminal config
+            sys.exit(f"Bye {name}! 👋")
+
+    # This is added to return the function
+    return play_gn
+
+    # We create a modules with args so that we can use the terminal to play the game and call the files
+
+
+if __name__ == "__main__":
+    import argparse
+
+    parser = argparse.ArgumentParser(
+        description="Provides a personalised game experience."
+    )
+
+    parser.add_argument(
+        "-n", "--name", metavar="name",
+        required=True, help="The name of the person playing the game."
+    )
+
+    args = parser.parse_args()
+
+    guess_number = gn(args.name)
+    guess_number()
