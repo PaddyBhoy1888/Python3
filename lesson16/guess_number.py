bf57e32958aee8ae24ecf08e28c0ea0a937c96a2
PaddyBhoy1888 <paddylltoi@hotmail.co.uk>
2024-01-02 13:04:51 +0000

    update 2.1.24

--- lesson16/guess_number.py
+++ lesson16/guess_number.py
@@ -41,6 +41,64 @@ def gn(name='playerOne'):
 
         if playerchoice not in ["1", "2", "3"]:
             print(f"{name}, please enter 1, 2, or 3.")
-            return play_rps()
-        # Create a player choice function where an input is required.
-        # A String is provided with the input to allow the user to determine 1 for Rock, 2 for Paper & 3 for Scissors
+            return play_gn()
+
+        player = int(playerchoice)
+        # As the above input would be a string we use the cast playerchoice from a string to an integer so that the code below can read if the players choice is between 1 - 3
+
+        computerchoice = random.choice("123")
+        # Computer to choose a random choice between one of the characters in the string.
+
+        computer = int(computerchoice)
+        # Here we change the computers choice from one of the random string values to an integer.
+
+        # define function 'decide_winner' with input parameters 'player' & 'computer'
+        def decide_winner(player, computer):
+            # We refer this function to the nonlocal variables listed below which are defined in the function gn local scope above.
+            nonlocal player_wins
+            nonlocal python_wins
+            nonlocal name
+
+            # Using an if & else statement we can state that if the player guesses the same as the computer then the player wins. Else any other outcome the computer wins.
+            if player == computer:
+                player_wins += 1  # If player wins increment the value by 1
+                # Return statement to say that the player wins.
+                return f"🐱‍🏍{name}, you Win
+
+            else:
+                python_wins += 1  # If computer wins increment the value by 1
+                # Return statement to say that the computer has won
+                return f"\n Sorry, {name}. Better luck next time.😢"
+                #A new variable to call the nested function 'decide_winner' refering to the parameters above inside the function.
+                
+        game_result = decide_winner(player, computer)
+
+        print(game_result)  # Print the output of 'game result'
+
+    # Creating a way for the code to keep count of how many times it has been played.
+    # We refer this variable to the nonlocal variables listed below which are defined in the function gn local scope above.
+        nonlocal game_count  # Refering to the nonlocal variable
+        game_count += 1  # Adding 1 everytime the game is played
+        # Print out a statment that counts by converting the game_count to a string
+        print(f"\n Game count: {game_count}")
+        # print out the winning count.
+        print(f"\n {name}'s wins: {player_wins}")
+        # print out the winning count.
+        print(f"\n python_wins: {python_wins}")
+        # Print out the winning percentage for player where percentage is calculated by (player_wins/game_count) at 2 decimal places 
+        print(f"\nYour winning percentage: {player_wins/game_count:.2%}")
+        # Print out the winning percentage for computer where percentage is calculated by (python_wins/game_count) at 2 decimal places 
+        print(f"\nYour winning percentage: {python_wins/game_count:.2%}")
+        # Print out play again statement.
+        print(f"\nPlay again, {name}?")
+        
+        # To play again we provide a while loop that must be True. 
+        while True:
+            # For the loop to continue the 'playagain' variable is created to allow the player to input a choice of Y or Q.
+            playagain = input("\n Y for yes or \n Q to Quit \n")
+            # If the player inputs a lower case we provide the .lower function so that python can understand the palyers input. 
+            if playagain.lower() not in ["y", "q"]:
+                continue
+            else:
+                break
+            # An if else statement is created so that unless the player choses one of the two inputs the question will be re asked until it receives the correct input.
